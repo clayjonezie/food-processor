40bfbd4be818f3956146818e8f15e07517758dd8
Clay Jones <clay@wealthfront.com>
2015-07-01 21:49:22 -0700

    add twitter wrapper and enter to sqlite

---
+++ twitter.py
@@ -0,0 +1,16 @@
+# pulls data from twitter 
+
+# see docs for this, but put a file in this dir
+# called keys with the values you see used
+import keys
+import tweepy
+
+auth = tweepy.OAuthHandler(keys.twitter_consumer_key, 
+                           keys.twitter_consumer_secret)
+auth.set_access_token(keys.twitter_access_token, 
+                      keys.twitter_access_token_secret)
+
+api = tweepy.API(auth)
+
+# todo count=1000 probably will not actually work
+tweets = api.user_timeline(screen_name=keys.twitter_screen_name, count=1000)
