ea5401a65faba9380dcdfb8d5d09cf76ac87b860
Clay Jones <clay@wealthfront.com>
2015-07-02 20:14:52 -0700

    add some bad words.. these will be user-specific. should add a cli to add a bad word, rather than hard coded

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+from nltk.corpus import stopwords
 from twitter import get_tweets
 
 import datetime
@@ -27,3 +28,25 @@ def read_tweets():
   cur = conn.cursor()
   rows = cur.execute('SELECT * FROM entries')
   return [(row[0], dateutil.parser.parse(row[1]), row[2]) for row in rows]
+
+def write_bad_words_db():
+  conn = sqlite3.connect('food-processor.db')
+  cur = conn.cursor()
+
+  create_table_sql = "CREATE TABLE bad_words (word text)"
+  try:
+    cur.execute(create_table_sql)
+  except sqlite3.OperationalError:
+    pass
+
+  bad_words = stopwords.words('english')
+  bad_words = bad_words + ["w", "ClayEatsFood", "cup", "x"]
+  bad_words = [(word,) for word in bad_words]
+  cur.executemany('INSERT INTO bad_words VALUES (?)', bad_words)
+  conn.commit()
+
+def read_bad_words():
+  conn = sqlite3.connect('food-processor.db')
+  cur = conn.cursor()
+  rows = cur.execute('SELECT * FROM bad_words')
+  return [row[0] for row in rows]
--- nlp.py
+++ nlp.py
@@ -1,10 +1,14 @@
 from nltk.tokenize import RegexpTokenizer
+from nltk.util import ngrams
 import db, re
 import operator
+from string import lower
 
 tweets = db.read_tweets()
 
 texts = [tweet[2] for tweet in tweets]
+
+bad_words = db.read_bad_words()
 
 def add_to_hist(d, word):
   if word in d.keys():
@@ -16,14 +20,17 @@ d = dict()
 tz = RegexpTokenizer(r'\w+')
 for text in texts:
   words = tz.tokenize(text)
-  for word in words:
-    if not re.match('^[0-9]*$', word):
-      add_to_hist(d, word)
+  words = [word for word in words if not re.match('^[0-9]*$', word)]
+  words = [word for word in words if not word in bad_words]
+  words = [lower(word) for word in words]
+  for ngram in list(ngrams(words, 3)) + list(ngrams(words, 2)) + list(ngrams(words, 1)):
+    word = " ".join(ngram)
+    add_to_hist(d, word)
 
 sorted_d = sorted(d.items(), key=operator.itemgetter(1))
 
 print sorted_d
 
 for word, n in sorted_d:
-  print word.ljust(20), '*' * n
+  print word.ljust(30), '*' * n
 
