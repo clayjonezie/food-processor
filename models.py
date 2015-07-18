3955f70ad120daef48a8bc3098a8d37816a2aef0
Clay Jones <clay@wealthfront.com>
2015-07-18 14:54:27 -0700

    adds login and user model

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.hybrid import hybrid_property
 from database import Base
+import my_bcrypt
 
 class Entry(Base):
     __tablename__ = 'entries'
@@ -13,4 +15,18 @@ class Entry(Base):
 
     def __repr__(self):
         return '<Entry: %r>' % self.content
-    
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(db.String(64), unique=True)
+    _password = Column(String(128))
+
+    @hybrid_property
+    def password(self):
+      return self._password
+
+    @password_setter
+    def _set_password(self, plaintext):
+      self._password = my_bcrypt.generate_password_hash(plaintext)
