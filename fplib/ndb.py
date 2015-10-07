f25a17d30b570035b74d696c3018df297fbb9bf9
clayjonezie <clay.mark.jones@gmail.com>
2015-10-06 20:36:39 -0700

    ndb

--- fplib/ndb.py
+++ fplib/ndb.py
@@ -1,4 +1,10 @@
 # functions to interface with the USDA National Nutrient Database
 # http://ndb.nal.usda.gov/ndb/doc/index
+import requests
 
 api_base = "http://api.nal.usda.gov/ndb"
+headers = {'Content-Type': 'application/json'}
+
+def get_food(ndbno):
+  r = requests.get(""Content-Type:application/json" -d '{"ndbno":"01009","type":"f"}' DEMO_KEY@api.nal.usda.gov/ndb/reports
+
